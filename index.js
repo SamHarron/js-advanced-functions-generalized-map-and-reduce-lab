81ed0672eeb1ae6af9558339d0e14397a61d512c
SamHarron <harronsam@gmail.com>
2021-09-04 17:21:31 -0400

    commit tree 1c21cb90ad0cd9d1d69e3086a0a3ea581c41ba3f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your functions here
+  
+function map(src, callback) {
+    const r = [];
+  
+    for (let i = 0; i < src.length; i++) {
+      const theElement = src[i];
+      r.push(callback(theElement));
+    }
+  
+    return r;
+  }
+  
+  function reduce(src, z, x) {
+    let total;
+    if (x) {
+      total = x;
+      for (let i = 0; i < src.length; i++) {
+        total = z(src[i], total);
+      }
+      return total;
+    } else {
+      total = src[0];
+      for (let i = 1; i < src.length; i++) {
+        total = z(src[i], total);
+      }
+      return total;
+    }
+  }
